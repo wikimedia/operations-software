5c28b3e5dd13f36d21c92df4b9b5ed20ca2bc115
Mark Bergsma <mark@wikimedia.org>
2012-12-20 21:43:32 +0100

    Implement copying of Swift metadata HTTP headers
    X-Object-Meta as well as other headers
    
    Change-Id: I11764ef0f393176a28ad79341ad1d5d4cf287418

--- swiftrepl/swiftrepl.py
+++ swiftrepl/swiftrepl.py
@@ -3,11 +3,12 @@
 # Written by Mark Bergsma <mark@wikimedia.org>
 
 import sys, re, collections, threading, time, traceback, httplib, socket, errno, random
-import cloudfiles
+import cloudfiles, cloudfiles.errors
 
 from Queue import Queue
 
 container_regexp = sys.argv[2] #"^wikipedia-en-local-thumb.[0-9a-f]{2}$"
+copy_headers = re.compile(r'^X-Content-Duration$')
 
 NOBJECT=100
 
@@ -29,22 +30,100 @@ def connect(params):
 		authurl=params['auth_url'],
 		timeout=60)
 
-def replicate_object(srcobj, dstobj, srcconnpool, dstconnpool):
-	dstobj.content_type = srcobj.content_type
-	dstobj.etag = srcobj.etag
-	dstobj.last_modified = srcobj.last_modified
-	dstobj.size = srcobj.size
-	dstobj.metadata = dict(srcobj.metadata)
-	
-	stream = srcobj.stream(chunksize=65536)
+def object_stream_prepare(obj, hdrs=None):
+	obj._name_check()
+	response = obj.container.conn.make_request('GET',
+	    path=[obj.container.name, obj.name], hdrs=hdrs)
+	if response.status < 200 or response.status > 299:
+		buff = response.read()
+		raise cloudfiles.errors.ResponseError(response.status, response.reason)
+	return response
 
+def object_stream(response, chunksize=8192):
+	buff = response.read(chunksize)
+	while len(buff) > 0:
+		yield buff
+		buff = response.read(chunksize)
+	# I hate you httplib
+	buff = response.read()
+
+def copy_metadata(response, dstobj, headers={}):
+	global copy_headers
+
+	for hdr in response.getheaders():
+		if copy_headers.match(hdr[0]):
+			headers[hdr[0]] = hdr[1]
+			print "Copying custom header", hdr[0], hdr[1]
+		elif hdr[0].lower().startswith('x-object-meta-'):
+			dstobj.metadata[hdr[0][14:]] = hdr[1]
+
+def send_object(dstobj, iterable, headers={}):
+	"""
+	Imported and modified from cloudfiles.storage_object.Object.send,
+	to allow specifying custom headers
+	"""
+	assert dstobj.size is not None
+	
+	from cloudfiles.utils import unicode_quote
+
+	if not dstobj.content_type:
+		dstobj.content_type = 'application/octet-stream'
+
+	path = "/%s/%s/%s" % (dstobj.container.conn.uri.rstrip('/'), \
+			unicode_quote(dstobj.container.name), unicode_quote(dstobj.name))
+	headers.update(dstobj._make_headers())
+	
+	headers['X-Auth-Token'] = dstobj.container.conn.token
+	headers['User-Agent'] = dstobj.container.conn.user_agent
+	http = dstobj.container.conn.connection
+	http.putrequest('PUT', path)
+	for key, value in headers.iteritems():
+		http.putheader(key, value)
+	http.endheaders()
+
+	response = None
+	transferred = 0
+	try:
+		for chunk in iterable:
+			http.send(chunk)
+			transferred += len(chunk)
+		# If the generator didn't yield enough data, stop, drop, and roll.
+		if transferred < dstobj.size:
+			raise cloudfiles.errors.IncompleteSend()
+		response = http.getresponse()
+		buff = response.read()
+	except cloudfiles.errors.timeout, err:
+		if response:
+			# pylint: disable-msg=E1101
+			response.read()
+		raise err
+
+	if (response.status < 200) or (response.status > 299):
+		raise cloudfiles.errors.ResponseError(response.status, response.reason)
+
+	for hdr in response.getheaders():
+		if hdr[0].lower() == 'etag':
+			dstobj._etag = hdr[1]
+
+def replicate_object(srcobj, dstobj, srcconnpool, dstconnpool):
 	# Replace the connections
 	srcobj.container.conn = srcconnpool.get()
 	dstobj.container.conn = dstconnpool.get()
+	
 	try:
 		for i in range(3):
 			try:
-				dstobj.send(stream)
+				# Start source GET request
+				response = object_stream_prepare(srcobj)
+
+				dstobj.content_type = srcobj.content_type
+				dstobj.etag = srcobj.etag
+				dstobj.last_modified = srcobj.last_modified
+				dstobj.size = srcobj.size
+				dstobj.metadata = dict(srcobj.metadata)
+				headers = {}
+				copy_metadata(response, dstobj, headers)
+				send_object(dstobj, object_stream(response, chunksize=65536), headers)
 			except httplib.CannotSendRequest as e:
 				srcobj.container.conn = srcconnpool.get()
 				dstobj.container.conn = dstconnpool.get()
