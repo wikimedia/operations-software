728222197e7877f9a70f0b2a0e3c2eacb9548380
Sean Pringle <springle@wikimedia.org>
2014-07-10 21:33:47 +1000

    Use MariaDB event scheduler on coredb slaves.
    The first jobs monitor, log, and potentially kill:
    
    1. slow wikiuser queries running over 300s
    2. sleeping wikiuser uncommitted transactions over 300s
    3. unsustainable max_connections traffic spikes over 10s
    
    They shall not touch wikiadmin, root, system users, or anything
    running legitimate slow queries (dumps, etc), plus they shall be
    conservative by default. 300s is an initial starting bar so that
    we tread softly; it should realistically be dropped to 60s or
    less. #3 is the biggest stick and only likely to come into play
    during an outage.
    
    The functionality has previously been a combination of pt-kill and
    custom scripts, or Opsen responding during an outage with manual
    KILL carnage. Those options all rely on a server being accessible
    for one or more new connections during an outage, and that the
    reserved mysqld SUPER connection slot is not in use or blocked.
    
    Using the built-in event scheduler:
    
    - Removes the need for separate cron jobs or polling services
    - Improves the chances of an overloaded box recovering itself
    - Reduces system load, network activity, use-of-perl, etc
    - Is cool :-)
    
    Change-Id: I8546406267887b073a81e713aa3dd38afce9c849

---
+++ dbtools/events_coredb_slave.sql
@@ -0,0 +1,251 @@
+-- Events for s[1-7] slaves
+
+set @cache_sql_log_bin := @@session.sql_log_bin;
+set @@session.sql_log_bin = 1;
+
+set @cache_event_scheduler := @@global.event_scheduler;
+set @@global.event_scheduler = 0;
+
+create database if not exists ops;
+
+use ops;
+
+-- Remember, table is replicated!
+-- https://wikitech.wikimedia.org/wiki/MariaDB#Schema_Changes
+
+drop table if exists event_log;
+create table event_log (
+  server_id int unsigned  not null,
+  stamp     datetime      not null,
+  event     varchar(100)  not null,
+  content   varchar(1024) not null,
+  index server_stamp (server_id, stamp)
+) engine=innodb default charset=binary;
+
+-- Avoid replicating event DDL. Coredb events should only be created
+-- on s[1-7] though they may rely on replicated tables like event_log.
+
+set @@session.sql_log_bin = 0;
+
+delimiter ;;
+
+-- Housekeeping
+
+drop event if exists wmf_slave_purge;;
+
+create event wmf_slave_purge
+
+    on schedule every 15 minute starts date(now())
+
+    do begin
+
+        -- If using statement based replication the @@server_id is unsafe. Convert it to a constant.
+        set @sql := concat(
+            'delete from event_log where stamp < now() - interval 1 day and server_id = ', @@server_id
+        );
+
+        prepare stmt from @sql; execute stmt; deallocate prepare stmt;
+
+    end ;;
+
+-- NOTE: information_schema.processlist
+-- MariaDB must generate information_schema.processlist every time it is accessed.
+-- That involves various locks which start to matter during high traffic, so stagger
+-- events appropriately, at least several seconds apart.
+
+-- wikiuser slow queries get killed at 300s
+-- time < 1000000 mariadb 5.5 bug allows new connections to be 2147483647 briefly
+
+drop event if exists wmf_slave_wikiuser_slow;;
+
+create event wmf_slave_wikiuser_slow
+
+    on schedule every 30 second starts date(now()) + interval 3 second
+
+    do begin
+
+        declare all_done int default 0;
+        declare thread_id bigint default null;
+        declare thread_query varchar(100);
+
+        declare slow_queries cursor for
+            select ps.id, substring(info,1,100)
+            from information_schema.processlist ps
+            where ps.command = 'Query'
+                and ps.user = 'wikiuser'
+                and ps.time between 300 and 1000000
+                and ps.info is not null
+                and lower(ps.info) regexp '^[[:space:]]*select'
+                and not lower(ps.info) regexp 'wikiexporter'
+                and not lower(ps.info) regexp 'master_pos_wait'
+            order by ps.time desc;
+
+        declare continue handler for not found set all_done = 1;
+
+        if (get_lock('wmf_slave_wikiuser_slow', 1) = 0) then
+            signal sqlstate value '45000' set message_text = 'get_lock';
+        end if;
+
+        set all_done = 0;
+        open slow_queries;
+
+        repeat fetch slow_queries into thread_id, thread_query;
+
+            if (thread_id is not null) then
+
+                kill thread_id;
+
+                insert into event_log values (@@server_id, now(), 'wmf_slave_wikiuser_slow',
+                    concat('kill ',thread_id, '; ',thread_query)
+                );
+
+            end if;
+
+            until all_done
+        end repeat;
+
+        close slow_queries;
+
+        -- https://mariadb.atlassian.net/browse/MDEV-4602
+        select 1 from (select 1) as t;
+
+        do release_lock('wmf_slave_wikiuser_slow');
+
+    end ;;
+
+-- wikiuser sleepers get killed at 300s
+-- time < 1000000 mariadb 5.5 bug allows new connections to be 2147483647 briefly
+
+drop event if exists wmf_slave_wikiuser_sleep;;
+
+create event wmf_slave_wikiuser_sleep
+
+    on schedule every 30 second starts date(now()) + interval 5 second
+
+    do begin
+
+        declare all_done int default 0;
+        declare thread_id bigint default null;
+
+        declare threads_sleeping cursor for
+            select ps.id
+            from information_schema.processlist ps
+            where ps.command = 'Sleep'
+                and ps.user = 'wikiuser'
+                and ps.time between 300 and 1000000
+                and ps.info is null
+            order by ps.time desc;
+
+        declare continue handler for not found set all_done = 1;
+
+        if (get_lock('wmf_slave_wikiuser_sleep', 1) = 0) then
+            signal sqlstate value '45000' set message_text = 'get_lock';
+        end if;
+
+        set all_done = 0;
+        open threads_sleeping;
+
+        repeat fetch threads_sleeping into thread_id;
+
+            if (thread_id is not null) then
+
+                kill thread_id;
+
+                insert into event_log values (@@server_id, now(), 'wmf_slave_wikiuser_sleep',
+                    concat('kill ',thread_id)
+                );
+
+            end if;
+
+            until all_done
+        end repeat;
+
+        close threads_sleeping;
+
+        -- https://mariadb.atlassian.net/browse/MDEV-4602
+        select 1 from (select 1) as t;
+
+        do release_lock('wmf_slave_wikiuser_sleep');
+
+    end ;;
+
+-- Identify a general overload
+
+drop event if exists wmf_slave_overload;;
+
+create event wmf_slave_overload
+
+    on schedule every 10 second starts date(now()) + interval 1 second
+
+    do begin
+
+        declare all_done int default 0;
+        declare thread_id bigint default null;
+        declare thread_query varchar(100);
+        declare active_count bigint default null;
+
+        declare active_queries cursor for
+            select ps.id, substring(info,1,100)
+            from information_schema.processlist ps
+            where ps.command = 'Query'
+                and ps.user = 'wikiuser'
+                and ps.time between 10 and 1000000
+                and ps.info is not null
+                and lower(ps.info) regexp '^[[:space:]]*select'
+                and not lower(ps.info) regexp 'wikiexporter'
+                and not lower(ps.info) regexp 'master_pos_wait'
+            order by ps.time desc
+            limit 100;
+
+        declare continue handler for not found set all_done = 1;
+
+        if (get_lock('wmf_slave_overload', 1) = 0) then
+            signal sqlstate value '45000' set message_text = 'get_lock';
+        end if;
+
+        set active_count := (
+            select count(ps.id)
+            from information_schema.processlist ps
+            where ps.command = 'Query'
+                and ps.info is not null
+        );
+
+        -- While a server usually has hundreds of open client connections, the number
+        -- of concurrent active queries is much lower during normal load. If we find a
+        -- spike, be nastier than normal and kill the slowest running over 30s.
+
+        if (active_count is not null and active_count > 300) then
+
+            set all_done = 0;
+            open active_queries;
+
+            repeat fetch active_queries into thread_id, thread_query;
+
+                if (thread_id is not null) then
+
+                    kill thread_id;
+
+                    insert into event_log values (@@server_id, now(), 'wmf_slave_overload (300)',
+                        concat('kill ',thread_id,'; ',thread_query)
+                    );
+
+                end if;
+
+                until all_done
+            end repeat;
+
+            close active_queries;
+
+        end if;
+
+        -- https://mariadb.atlassian.net/browse/MDEV-4602
+        select 1 from (select 1) as t;
+
+        do release_lock('wmf_slave_overload');
+
+    end ;;
+
+delimiter ;
+
+set @@session.sql_log_bin = @cache_sql_log_bin;
+set @@global.event_scheduler = @cache_event_scheduler;
