98b64cdfadab6cc511476078a18fc0b44f2b6af6
Sean Pringle <springle@wikimedia.org>
2014-07-10 14:12:57 +1000

    Use MariaDB event scheduler for labsdb.
    Start by checking for and killing threads that:
    
    - Are normal labs users /^[usp][0-9]+/
    - Have been in Sleep state for over 300s
    - Have an open, uncommitted transaction in InnoDB
    
    This sort of thing happens when people use the RDBMS as locking
    mechanism for something external, or don't issue a COMMIT with a
    connection pool or some sort, or client connections fail in various
    obscure ways that don't timeout the TCP socket. The other safety
    net is wait_timeout=28800, which is, necessarily, too far off to
    help with uncommitted transactions.
    
    Change-Id: Id792f752339d46cdc8124dc471b0d8d0897fad5a

---
+++ dbtools/events_labsdb.sql
@@ -0,0 +1,87 @@
+-- Events for labsdb100[123]
+
+set @cache_sql_log_bin := @@session.sql_log_bin;
+set @@session.sql_log_bin = 1;
+
+create database if not exists ops;
+
+use ops;
+
+-- Remember, table is replicated!
+-- https://wikitech.wikimedia.org/wiki/MariaDB#Schema_Changes
+
+drop table if exists event_log;
+create table event_log (
+  server_id int unsigned  not null,
+  stamp     datetime      not null,
+  event     varchar(100)  not null,
+  content   varchar(1024) not null,
+  index server_stamp (server_id, stamp)
+) engine=innodb default charset=binary;
+
+-- Avoid replicating event DDL. Labs events should only be created on labsdb
+-- though they may rely on replicated tables like event_log.
+
+set @@session.sql_log_bin = 0;
+
+delimiter ;;
+
+-- Housekeeping
+
+drop event if exists wmf_labs_purge;;
+
+create event wmf_labs_purge
+
+    on schedule every 15 minute starts date(now())
+
+    do begin
+
+        delete from event_log
+        where stamp < now() - interval 1 day
+            and server_id = @@server_id
+        limit 1000;
+
+    end ;;
+
+-- Kill connections from labs users sleeping for over 300s with an open transaction.
+-- These cause innodb purge lag and can block replication, DDL metadata locking, etc.
+-- This /does not/ affect users legitimately running slow queries, or maintaining
+-- persistent connections, for more than 300s.
+
+drop event if exists wmf_labs_sleepers_txn;;
+
+create event wmf_labs_sleepers_txn
+
+    on schedule every 10 second starts date(now()) + interval 1 second
+
+    do begin
+
+        declare thread_id bigint;
+
+        set thread_id := ( select ps.id
+            from information_schema.processlist ps
+            inner join information_schema.innodb_trx trx
+                on ps.id = trx.trx_mysql_thread_id
+            where ps.command = 'Sleep'
+                and ps.time between 300 and 1000000
+                and ps.user regexp '^[usp][0-9]+'
+                and ps.user not regexp '^(system|root|dbmanager|watchdog)'
+            order by ps.time desc
+            limit 1
+        );
+
+        if (thread_id is not null) then
+
+            kill thread_id;
+
+            insert into event_log values (@@server_id, now(), 'wmf_labs_sleepers_txn',
+                concat('kill ',thread_id)
+            );
+
+        end if;
+
+    end ;;
+
+delimiter ;
+
+set @@session.sql_log_bin = @cache_sql_log_bin;
