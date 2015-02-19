b418893efcdb1a56f31d73400119404a4fab182e
Tim Landscheidt <tim@tim-landscheidt.de>
2015-02-19 19:29:23 +0000

    Add txt2yaml
    txt2yaml is a tiny utility to convert line-oriented text files or
    pipes to YAML arrays and insert/replace them in (existing) YAML
    documents.  In Unix fashion, multiple invocations can be chained to
    update multiple keys:
    
    | $ txt2yaml key_1 file_1.txt < yaml.old |
    | > txt2yaml key_2 file_2.txt            |
    | > txt2yaml key_3 file_3.txt > yaml.new
    
    Change-Id: Id8b91cabe60249aa354b459f0d4fa4fa3ca9745f

---
+++ txt2yaml/txt2yaml.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+
+import argparse
+import sys
+import yaml
+
+def main():
+    # Parse command line arguments.
+    parser = argparse.ArgumentParser(description='Convert text file to YAML.')
+    parser.add_argument('key', metavar='KEY', help='key to insert/replace')
+    parser.add_argument('file', metavar='FILE', help='text file to read')
+    args = parser.parse_args()
+
+    # Read YAML from stdin.
+    y = yaml.load(sys.stdin)
+
+    # Read text file.
+    with open(args.file, 'r') as f:
+        y[args.key] = map(lambda line: line.rstrip('\n'), f.readlines())
+
+    # Write amended YAML to stdout.
+    yaml.dump(y, sys.stdout)
+
+if __name__ == '__main__':
+    main()
