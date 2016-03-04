b96d47fdb5c2a49dfde43368c319ba549d84ac12
Jaime Crespo <jcrespo@wikimedia.org>
2016-03-04 11:20:37 +0100

    Simplify lag checking thanks to new pt-heartbeat-wikimedia
    Change-Id: Ib7e050627354c25cbbd9caa225cdb8d048f2c856

--- maintain-replicas/heartbeat-views.sql
+++ maintain-replicas/heartbeat-views.sql
@@ -7,19 +7,11 @@ ALGORITHM=UNDEFINED
 DEFINER=`root`@`localhost`
 SQL SECURITY DEFINER VIEW
 `heartbeat_p`.`heartbeat` AS
-SELECT (
-CASE
-WHEN (`heartbeat`.`heartbeat`.`file` like 'db1052%') THEN 's1'
-WHEN (`heartbeat`.`heartbeat`.`file` like 'db1018%') THEN 's2'
-WHEN (`heartbeat`.`heartbeat`.`file` like 'db1038%') THEN 's3'
-WHEN (`heartbeat`.`heartbeat`.`file` like 'db1040%') THEN 's4'
-WHEN (`heartbeat`.`heartbeat`.`file` like 'db1058%') THEN 's5'
-WHEN (`heartbeat`.`heartbeat`.`file` like 'db1023%') THEN 's6'
-WHEN (`heartbeat`.`heartbeat`.`file` like 'db1033%') THEN 's7'
-ELSE 'unknown' end
-) AS `shard`,
-`heartbeat`.`heartbeat`.`ts` AS `last_updated`,
-timestampdiff(SECOND,`heartbeat`.`heartbeat`.`ts`,utc_timestamp()) AS `lag`
-FROM `heartbeat`.`heartbeat`;
+SELECT
+`shard` AS `shard`,
+max(`heartbeat`.`heartbeat`.`ts`) AS `last_updated`,
+timestampdiff(SECOND, max(`heartbeat`.`heartbeat`.`ts`), utc_timestamp()) AS `lag`
+FROM `heartbeat`.`heartbeat`
+GROUP BY `shard`;
 -- Change SECOND precission to MICROSECOND when this bug disappears:
 -- https://mariadb.atlassian.net/browse/MDEV-9175
