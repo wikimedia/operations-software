2931cd9f7c3d118d0a8c47d54dde33c81c766999
Dzahn <dzahn@wikimedia.org>
2016-03-11 13:28:10 -0800

    salt-misc: set bastion host based on realm as $2
    Add an optional second parameter $2/$realm which sets
    the correct bastion host whether we are a root in prod,
    a root in labs, a non-root in prod or a non-root in labs.
    
    Change-Id: I0c1857f6d5296514067e58d76ad4cbb1fd340343

--- salt-misc/gather-minion-info.sh
+++ salt-misc/gather-minion-info.sh
@@ -9,20 +9,35 @@ if [ -z "$SSH" ]; then
 fi
 
 if [ -z "$1" ]; then
-    echo "Usage: $0 filename"
+    echo "Usage: $0 filename [realm]"
     echo "filename should have list of fqdn of instances to check, one per line"
+    echo "realm is optional and should be one of 'ops', 'prod', 'opslabs' or 'labs'. default is opslabs"
     exit 1
 fi
+
+case "$2" in
+ 'ops')
+    BASTION="iron.wikimedia.org" ;;
+ 'prod')
+    BASTION="bast1001.wikimedia.org" ;;
+ 'opslabs')
+    BASTION="bastion-restricted.wmflabs.org" ;;
+ 'labs')
+    BASTION="bastion.wmflabs.org" ;;
+ *)
+    BASTION="bastion-restricted.wmflabs.org" ;;
+esac
+
 hostlist=`cat $1`
 
 for minion in $hostlist; do
-    echo "doing $minion"
-        $SSH bastion-restricted.wmflabs.org ssh -l root -o 'StrictHostKeyChecking=no' "$minion" "ls -lt /var/log/salt/minion"
-        $SSH bastion-restricted.wmflabs.org ssh -l root -o 'StrictHostKeyChecking=no' "$minion" "tail -20 /var/log/salt/minion"
-        $SSH bastion-restricted.wmflabs.org ssh -l root -o 'StrictHostKeyChecking=no' "$minion" "cat /etc/salt/minion"
-        $SSH bastion-restricted.wmflabs.org ssh -l root -o 'StrictHostKeyChecking=no' "$minion" 'ps axuww | grep salt | grep -v axuww'
-        $SSH bastion-restricted.wmflabs.org ssh -l root -o 'StrictHostKeyChecking=no' "$minion" "dpkg -l | grep salt"
-        $SSH bastion-restricted.wmflabs.org ssh -l root -o 'StrictHostKeyChecking=no' "$minion" "cat /etc/issue"
-        $SSH bastion-restricted.wmflabs.org ssh -l root -o 'StrictHostKeyChecking=no' "$minion" "ls -lt /var/log/puppet.log"
-        $SSH bastion-restricted.wmflabs.org ssh -l root -o 'StrictHostKeyChecking=no' "$minion" "tail -40 /var/log/puppet.log"
+    echo "doing $minion - using bastion $BASTION"
+        $SSH $BASTION ssh -l root -o 'StrictHostKeyChecking=no' "$minion" "ls -lt /var/log/salt/minion"
+        $SSH $BASTION ssh -l root -o 'StrictHostKeyChecking=no' "$minion" "tail -20 /var/log/salt/minion"
+        $SSH $BASTION ssh -l root -o 'StrictHostKeyChecking=no' "$minion" "cat /etc/salt/minion"
+        $SSH $BASTION ssh -l root -o 'StrictHostKeyChecking=no' "$minion" "ps axuww | grep salt | grep -v axuww"
+        $SSH $BASTION ssh -l root -o 'StrictHostKeyChecking=no' "$minion" "dpkg -l | grep salt"
+        $SSH $BASTION ssh -l root -o 'StrictHostKeyChecking=no' "$minion" "cat /etc/issue"
+        $SSH $BASTION ssh -l root -o 'StrictHostKeyChecking=no' "$minion" "ls -lt /var/log/puppet.log"
+        $SSH $BASTION ssh -l root -o 'StrictHostKeyChecking=no' "$minion" "tail -40 /var/log/puppet.log"
 done
