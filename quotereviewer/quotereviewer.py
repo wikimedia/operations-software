7dbab6fbbd1c4213e884e41ed7509c5c6c67ef4c
Riccardo Coccioli <rcoccioli@wikimedia.org>
2021-08-26 15:08:32 +0200

    quotereviewer: add support for portal quotes
    * The Dell's portal-generated quotes differ substantially from the ones
      we were used to receive. Add support for those too.
    * Because we expect the majority of the new quotes to be
      portal-generated try first to parse them with the new logic and then
      fallback to the old one.
    * The new format doesn't parse easily with the -layout option for
      pdftotext (physical=True in the python bindings) due to SKU splitted
      between lines. Also because it has multiple SKUs per single item, as
      opposed to the old format. For this reason parsing the new format
      without the -layout option (physical=False).
    * The previous behaviour of working both with just the CLI for pdftotext
      installed or also the Python bindings has been kept.
    
    Bug: T288354
    Change-Id: I2c05f5eba62c4846ca167f1ab56278d6d7cac6fd

--- quotereviewer/quotereviewer.py
+++ quotereviewer/quotereviewer.py
@@ -36,6 +36,9 @@ except ImportError:
     LOG_FORMAT = "%(message)s"
 
 
+FORMAT_IDENTIFIER = 'Your Shopping Cart'
+
+
 class ConfigParseError(Exception):
     """Configuration file parsing error."""
 
@@ -51,29 +54,28 @@ class PdfToTextNotFound(Exception):
 try:
     import pdftotext
 
-    def extract_text_from_pdf(pdf_file):
+    def extract_text_from_pdf(pdf_file, physical=False):
         """Extract text from a PDF file handle; return a list of pages."""
         try:
-            pdf = pdftotext.PDF(pdf_file)
+            pdf = pdftotext.PDF(pdf_file, physical=physical)
         except pdftotext.Error as exc:
             raise PdfParseError("Unable to PDF -> text: " + str(exc))
         return pdf
-
 
 except ImportError:
     import os
     import subprocess
     import textwrap
 
-    def extract_text_from_pdf(pdf_file):
+    def extract_text_from_pdf(pdf_file, physical=False):
         """Extract text from a PDF file handle; return a list of pages."""
+        command = ["pdftotext"]
+        if physical:
+            command.append("-layout")
+
+        command += ["-enc", "UTF-8", "-", "-"]
         try:
-            subp = subprocess.run(
-                ["pdftotext", "-layout", "-enc", "UTF-8", "-", "-"],
-                stdin=pdf_file,
-                stdout=subprocess.PIPE,
-                stderr=subprocess.PIPE,
-            )
+            subp = subprocess.run(command, stdin=pdf_file, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
             subp.check_returncode()
         except subprocess.CalledProcessError as exc:
             err = subp.stderr.decode("utf8", "ignore").strip()
@@ -92,16 +94,24 @@ except ImportError:
 
 def parse_pdf(pdf_file):
     """Parse the PDF quote, extract all SKUs and return them in a dict."""
+    pdf = extract_text_from_pdf(pdf_file)
+    if any(FORMAT_IDENTIFIER in page for page in pdf):
+        return parse_portal_pdf(pdf)
+    else:
+        pdf_file.seek(0)
+        pdf = extract_text_from_pdf(pdf_file, physical=True)
+        return parse_legacy_pdf(pdf)
+
+
+def parse_legacy_pdf(pdf):
+    """Parse the PDF quote, extract all SKUs and return them in a dict."""
     item_res = [
         # ~2018-style quotes
         r"(?P<sku>\d{3}-[0-9A-Z]{4}) +(?P<description>.+?) +(\d+) +- +-",
         # ~2019-style quotes
         r" +(?P<description>.+?) +(?P<sku>\d{3}-[0-9A-Z]{4}) + - + (\d+) +-",
     ]
-
     parsed = {}
-
-    pdf = extract_text_from_pdf(pdf_file)
     for page in pdf:
         for line in page.splitlines():
             # check against the multiple regexps
@@ -115,6 +125,50 @@ def parse_pdf(pdf_file):
                 continue
 
             parsed[match.group("sku")] = match.group("description")
+
+    return parsed
+
+
+def parse_portal_pdf(pdf):
+    parsed = {}
+    for page in pdf:
+        if not page:
+            continue
+
+        tokens = []
+        in_token = False
+        for line in page.splitlines():
+            if not line:
+                in_token = False
+                continue
+
+            if not in_token:
+                tokens.append(line)
+            else:
+                if tokens[-1] == FORMAT_IDENTIFIER:  # Missing separator on new page
+                    tokens.append(line)
+                else:
+                    last = tokens.pop()
+                    tokens.append(last + f' {line}')
+
+            in_token = True
+
+        start = tokens.index('Category')
+        headers = ['Category', 'Description', 'Code', 'SKU', 'ID']
+        key_index = 3
+        value_index = 1
+        batch = len(headers)
+        if tokens[start:start+batch] != headers:
+            raise PdfParseError('Unable to find the table headers')
+
+        for i in range(start+batch, len(tokens), batch):
+            line = tokens[i:i+batch]
+            if len(line) < batch:
+                continue  # Reached end of page
+
+            skus = re.findall(r'\[(\d{3}-[0-9A-Z]{4})\]', line[key_index])
+            for sku in skus:
+                parsed[sku] = line[value_index]
 
     return parsed
 
