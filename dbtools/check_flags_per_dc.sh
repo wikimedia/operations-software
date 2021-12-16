5d0f24b34e846724b6dcfd26b1a82f38c9332bf1
Manuel Arostegui <marostegui@wikimedia.org>
2021-12-16 07:23:21 +0100

    check_flags_per_dc.sh: Replace mysql.py with db-mysql
    db-mysql is the new wrapper.
    
    Bug: T297618
    Change-Id: Ie4a592ad71ae10c1376f8b5aafb26bdbeb4eac46

--- dbtools/check_flags_per_dc.sh
+++ dbtools/check_flags_per_dc.sh
@@ -9,14 +9,14 @@ for i in s1 s2 s3 s4 s5 s6 s7 s8 x1 x2 pc1 pc2 pc3 es1 es2 es3 es4 es5
 do
 
 echo "== Check event_scheduler=OFF for $i in $DC ==="
-/home/marostegui/section $i | grep -v clouddb | grep $DC | while read host port; do mysql.py -h$host:$port -e "show global variables like 'event_scheduler'" | grep -v Variable_name | grep -v ON; done
+/home/marostegui/section $i | grep -v clouddb | grep $DC | while read host port; do db-mysql $host:$port -e "show global variables like 'event_scheduler'" | grep -v Variable_name | grep -v ON; done
 
 echo "== Check for non installed query killers in $i $DC (5=OK, 3=OK (masters) ==="
-/home/marostegui/section $i | grep -v clouddb | grep -v pc | grep $DC | while read host port; do mysql.py -h$host:$port ops -e "show events" | wc -l ; done
+/home/marostegui/section $i | grep -v clouddb | grep -v pc | grep $DC | while read host port; do db-mysql $host:$port ops -e "show events" | wc -l ; done
 
 echo "== Check for DISABLED events in $i $DC  ==="
-/home/marostegui/section $i | grep -v clouddb | grep -v pc | grep $DC | while read host port; do mysql.py -h$host:$port ops -e "show events"| grep DISABLED ; done
+/home/marostegui/section $i | grep -v clouddb | grep -v pc | grep $DC | while read host port; do db-mysql $host:$port ops -e "show events"| grep DISABLED ; done
 
 echo "== Check for disabled GTID in $i $DC  ==="
-/home/marostegui/section $i | grep $DC | while read host port; do mysql.py -h$host:$port -e "show slave status\G"| grep Using | grep -v Slave_Pos; done
+/home/marostegui/section $i | grep $DC | while read host port; do db-mysql $host:$port -e "show slave status\G"| grep Using | grep -v Slave_Pos; done
 done
