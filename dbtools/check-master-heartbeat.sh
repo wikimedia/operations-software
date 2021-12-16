13f7c11804f4a1f7522b3755a5eb05bd3f7b8a7b
Stevie Beth Mhaol <sbmhaol@wikimedia.org>
2021-12-16 11:41:23 +0100

    check-master-heartbeat.sh: Switch to db-mysql
    Bug: T297618
    Change-Id: I8c5324799f990d392d1d8b12724eb70bfcb8d6aa

--- dbtools/check-master-heartbeat.sh
+++ dbtools/check-master-heartbeat.sh
@@ -17,17 +17,17 @@ section="${1:?}"; shift
 src_dc="${1:?}"; shift
 dest_dc="${1:?}"; shift
 
-src_master=$(sudo -H mysql.py -A -BN -h $ZARCILLO_HOST zarcillo -e "select instance from masters where section='$section' and dc='$src_dc'")
+src_master=$(sudo db-mysql $ZARCILLO_HOST -BN -A zarcillo -e "select instance from masters where section='$section' and dc='$src_dc'")
 [ -n "$src_master" ] || { echo "ERROR: no master found for section $section in DC $src_dc"; exit 1; }
 
-src_master_fqdn=$(sudo -H mysql.py -A -BN -h $ZARCILLO_HOST zarcillo -e "select server from instances where name='$src_master'")
+src_master_fqdn=$(sudo db-mysql $ZARCILLO_HOST -BN -A zarcillo -e "select server from instances where name='$src_master'")
 [ -n "$src_master_fqdn" ] || { echo "ERROR: unable to find fqdn for $src_master"; exit 1; }
 
 src_master_ip=$(getent hosts $src_master_fqdn | cut -f1 -d' ') || { echo "ERROR: unable to get IP of $src_master_fqdn"; exit 1; }
 echo Source: $src_master_fqdn $src_master_ip
 
-dest_master=$(sudo -H mysql.py -A -BN -h $ZARCILLO_HOST zarcillo -e "select instance from masters where section='$section' and dc='$dest_dc'")
+dest_master=$(sudo db-mysql $ZARCILLO_HOST -BN -A zarcillo -e "select instance from masters where section='$section' and dc='$dest_dc'")
 [ -n "$dest_master" ] || { echo "ERROR: no master found for section $section in DC $dest_dc"; exit 1; }
 echo Dest: $dest_master
 
-sudo -H mysql.py -A -h $dest_master heartbeat -e "select * from heartbeat where server_id=inet_aton('$src_master_ip') order by ts desc limit 1"
+sudo db-mysql $dest_master -A heartbeat -e "select * from heartbeat where server_id=inet_aton('$src_master_ip') order by ts desc limit 1"
