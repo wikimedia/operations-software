62a91d43ef056842e7e22510e27c87c961948bbc
Amir Sarabadani <ladsgroup@gmail.com>
2021-12-20 14:36:18 +0100

    auto_schema: Refactor bash to make it a bit cleaner
     - Move logic of dbctl to its own class.
     - Move subprocess stuff to run_internal. Possibly can be cleaned up
       better later.
    
    Bug: T288235
    Change-Id: I6118b2d34f300db3187b664c143a19af17f29fc2

--- dbtools/auto_schema/auto_schema/bash.py
+++ dbtools/auto_schema/auto_schema/bash.py
@@ -7,39 +7,53 @@ from datetime import datetime
 
 def run(command):
     print(datetime.fromtimestamp(time.time()), command)
-    if '--run' in sys.argv or 'show slave' in command:
-        if command.startswith(
-                'dbctl ') and not command.startswith('dbctl config '):
-            diff_res = 'Something'
-            while diff_res:
-                diff_res = run('dbctl config diff')
-                if diff_res:
-                    print(diff_res)
-                    print('Waiting for the dbctl to clear up')
-                    time.sleep(5)
-        res = subprocess.run(command, capture_output=True, shell=True)
-        if command.startswith(
-                'dbctl ') and not command.startswith('dbctl config '):
-            run('dbctl config diff')
-        stderr = res.stderr.decode('utf-8')
-        stderr_cleaned = re.sub(r'\s', '', stderr)
-        if stderr_cleaned:
-            print('ERROR: ')
-            print('--------')
-            print(stderr)
-            print('--------')
-        else:
-            stderr = ''
-        stdout = res.stdout.decode('utf-8')
-        print('STDOUT')
-        stdout_cleaned = re.sub(r'\s', '', stdout)
-        if stdout_cleaned:
-            print(stdout)
-        else:
-            stdout = ''
+    if '--run' not in sys.argv and 'show slave' not in command:
+        return ''
+    if command.startswith('dbctl '):
+        return run_dbctl(command)
+    return run_internal(command)
 
-        output = stdout
-        if stderr_cleaned:
-            output += '\nerror:\n' + stderr
-        return output
-    return ''
+
+def run_dbctl(command):
+    if '--run' not in sys.argv:
+        return ''
+    # Make sure you get a clean dbctl before changing config
+    if not command.startswith('dbctl config '):
+        diff_res = 'Something'
+        while diff_res:
+            diff_res = run_dbctl('dbctl config diff')
+            if diff_res:
+                print(diff_res)
+                print('Waiting for dbctl to clear up')
+                time.sleep(5)
+    res = run_internal(command)
+    # Show config
+    if not command.startswith('dbctl config '):
+        run_dbctl('dbctl config diff')
+    return res
+
+
+def run_internal(command):
+    """Do not call this function directly."""
+    res = subprocess.run(command, capture_output=True, shell=True)
+    stderr = res.stderr.decode('utf-8')
+    stderr_cleaned = re.sub(r'\s', '', stderr)
+    if stderr_cleaned:
+        print('ERROR: ')
+        print('--------')
+        print(stderr)
+        print('--------')
+    else:
+        stderr = ''
+    stdout = res.stdout.decode('utf-8')
+    print('STDOUT')
+    stdout_cleaned = re.sub(r'\s', '', stdout)
+    if stdout_cleaned:
+        print(stdout)
+    else:
+        stdout = ''
+
+    output = stdout
+    if stderr_cleaned:
+        output += '\nerror:\n' + stderr
+    return output
