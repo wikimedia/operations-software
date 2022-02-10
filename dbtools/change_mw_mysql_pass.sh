adcaff23499361c3a8214735b03c970d8a3289a1
Manuel Arostegui <marostegui@wikimedia.org>
2022-02-10 09:44:27 +0100

    change_mw_mysql_pass.sh: Tendril is gone
    Bug: T297605
    Change-Id: Id9c3265b7f2163c15589b3081101407c64f05b45

--- dbtools/change_mw_mysql_pass.sh
+++ dbtools/change_mw_mysql_pass.sh
@@ -7,7 +7,7 @@ PASSWORD=""
 
 # Host and database to grab the hosts from
 TENDRIL_HOST="db1115.eqiad.wmnet"
-DATABASE="tendril"
+DATABASE="tendril" # Tendril is gone, this needs changing to zarcillo database and the query needs to be re-done to grab the list of hosts from there https://phabricator.wikimedia.org/T297605
 
 if [ -z "$USER" ] || [ -z "$PASSWORD" ]
 then
