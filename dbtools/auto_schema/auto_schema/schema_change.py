0fb386a28a5f94e853eed0d759215482249c7732
Amir Sarabadani <ladsgroup@gmail.com>
2022-03-31 18:35:16 +0200

    auto_schema: Wrap starting replication with finally
    There has a been case today where the check caused a fatal and as result
    the replication didn't get restarted leading to a page.
    
    Wrap start of replication with finally to make sure whatever happens, it
    at least won't keep it out of replication for too long.
    
    Also, letting result of check/schema change being returned.
    
    Change-Id: I974954ad3a8d6f64df50bf85b96f0150f6cd43a7

--- dbtools/auto_schema/auto_schema/schema_change.py
+++ dbtools/auto_schema/auto_schema/schema_change.py
@@ -47,6 +47,7 @@ class SchemaChange(object):
         self.logger.log_file('End of schema change on {}'.format(
             ','.join([i.host for i in self.replica_set.replicas])))
         self.logger.log_file('Result: ' + json.dumps(self.cases))
+        return self.cases
 
     def sql_on_each_replica(self, sql, check=None):
         for host in self.gen:
@@ -62,12 +63,14 @@ class SchemaChange(object):
             self.logger.log_file('Start of schema change sql on {}'.format(host.host))
             sql_for_this_host = sql
             host.run_sql('stop slave;')
-            if self.replica_set.change_without_replication(host):
-                sql_for_this_host = 'set session sql_log_bin=0; ' + sql_for_this_host
+            try:
+                if self.replica_set.change_without_replication(host):
+                    sql_for_this_host = 'set session sql_log_bin=0; ' + sql_for_this_host
 
-            res = host.run_sql(sql_for_this_host)
-            self.logger.log_file('End of schema change sql on {}'.format(host.host))
-            host.run_sql('start slave;')
+                res = host.run_sql(sql_for_this_host)
+                self.logger.log_file('End of schema change sql on {}'.format(host.host))
+            finally:
+                host.run_sql('start slave;')
 
             if 'error' in res.lower():
                 self.cases['errored'].append(host.host)
@@ -95,29 +98,37 @@ class SchemaChange(object):
 
     def run_sql_per_db(self, host, sql, check):
         res = ''
+        needed = False
         if not self.check_only:
             host.run_sql('stop slave;')
-        needed = False
-        for db_name in self.replica_set.get_dbs():
-            db = Db(host, db_name)
-            if check:
-                if check(db):
-                    self.logger.log_file('Already applied on {} in {}, skipping'.format(db_name, host.host))
-                    continue
-            needed = True
-            if self.check_only:
-                continue
-            res += db.run_sql(sql)
-            if check and '--run' in sys.argv:
-                if not check(db):
-                    self.logger.log_file('Schema change was not applied on {} in {}'.format(db_name, host.host))
-                    res += 'Error: Schema change was not applied'
-                else:
-                    self.logger.log_file('Schema change finished on {} in {}'.format(db_name, host.host))
-        if not needed:
-            self.cases['already done in all dbs'].append(host.host)
-        else:
-            self.cases['needed in some dbs'].append(host.host)
-        if not self.check_only:
-            host.run_sql('start slave;')
+        try:
+            for db_name in self.replica_set.get_dbs():
+                (needed_batch, res_batch) = self.handle_db(db_name, host, check, sql)
+                res += res_batch
+                needed = needed or needed_batch
+            if not needed:
+                self.cases['already done in all dbs'].append(host.host)
+            else:
+                self.cases['needed in some dbs'].append(host.host)
+        finally:
+            if not self.check_only:
+                host.run_sql('start slave;')
         return res
+
+    def handle_db(self, db_name, host, check, sql):
+        res = ''
+        db = Db(host, db_name)
+        if check:
+            if check(db):
+                self.logger.log_file('Already applied on {} in {}, skipping'.format(db_name, host.host))
+                return (False, res)
+        if self.check_only:
+            return (True, res)
+        res = db.run_sql(sql)
+        if check and '--run' in sys.argv:
+            if not check(db):
+                self.logger.log_file('Schema change was not applied on {} in {}'.format(db_name, host.host))
+                res += 'Error: Schema change was not applied'
+            else:
+                self.logger.log_file('Schema change finished on {} in {}'.format(db_name, host.host))
+        return (True, res)
