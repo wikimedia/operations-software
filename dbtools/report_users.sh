d14f514b12cdea6f0f667954d9d1fe2b10d652a3
Manuel Arostegui <marostegui@wikimedia.org>
2024-01-05 08:27:55 +0100

    report_users.sh: Improvements
    Changes to the existing script.
    This was long due...
    
    Change-Id: Ie35a4f18021f71533f25ca7dbfbf511fc550288f

--- dbtools/report_users.sh
+++ dbtools/report_users.sh
@@ -1,70 +1,89 @@
 #!/bin/bash
+
+# Set the environment variables
 PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 RETEVAL=0
-# DB where data will be stored
+# Database configurations
 DATABASE="zarcillo"
 TABLE="nil_grants"
 DB_HOST="db1215.eqiad.wmnet"
 DB_USER="root"
 
-# DB from where to get the list of live hosts
 SOURCE_DB_HOST="db1215.eqiad.wmnet"
 SOURCE_DB="zarcillo"
 SOURCE_DB_TABLE="instances"
-# Timeout in seconds
 DB_TIMEOUT=5
 
 MYSQL="`which db-mysql`"
 
-if [ ! -f "$MYSQL" ]
-then
-    echo "db-mysql is not present"
+# IPs to be whitelisted
+WHITELISTED_IPS=(
+    '10.192.0.129'
+    '10.192.16.9'
+    '10.192.32.137'
+    '10.64.16.14'
+    '10.64.37.27'
+    '10.64.32.180'
+    '10.64.32.179'
+    '10.192.48.47'
+    '10.64.0.15'
+    '10.64.32.10'
+    '10.64.16.7'
+    '10.64.48.33'
+    '10.64.130.15'
+    '10.64.134.16'
+    '10.64.151.2'
+    '10.64.150.4'
+)
+# Check if db-mysql is available
+if ! command -v "$MYSQL" &> /dev/null; then
+    echo "Command 'db-mysql' not found or not executable."
     exit 1
 fi
 
+# Handle script arguments
 case "$1" in
   "")
     echo "Usage: $0 --empty-password --unused-grants"
     RETVAL=1
     ;;
   --empty-password)
+    # Clear the table before starting
+    $MYSQL $DB_HOST $DATABASE -e "truncate table $TABLE;"
 
-# Empty the table before start
-$MYSQL $DB_HOST $DATABASE -e " truncate table $TABLE;"
+    # Fetch servers and ports, check for empty passwords
+    $MYSQL $SOURCE_DB_HOST $SOURCE_DB -e "select server,port from $SOURCE_DB_TABLE;" -BN | while read server port
+    do
+        # Construct the query for users with empty passwords, excluding whitelisted IPs
+        exclude_hosts=""
+        for ip in "${WHITELISTED_IPS[@]}"
+        do
+            exclude_hosts+="and host !='$ip' "
+        done
 
-$MYSQL $SOURCE_DB_HOST $SOURCE_DB -e "select server,port from $SOURCE_DB_TABLE;" -BN | while read server port
-do
+        $MYSQL $server:$port -e "select User,Host from mysql.user where password='' and plugin !='unix_socket' and user !='labsdbuser' and user !='research_role' and user !='mariadb.sys' $exclude_hosts" -BN | while read user host
+        do
+            echo "set session binlog_format=row; INSERT INTO $TABLE VALUES ('${server}','${port}','${user}','${host}',NOW()) ON DUPLICATE KEY UPDATE last_update = NOW();" | $MYSQL $DB_HOST -u $DB_USER $DATABASE
+        done
+    done
 
-# The following IPs are whitelisted as they are proxies and we cannot remove haproxy or set a password for it see: T199061#4426646
+    # Check and report existing users with empty passwords
+    USERS_COUNT=$($MYSQL $DB_HOST -u $DB_USER $DATABASE -e "select count(*) from nil_grants;" -BN)
 
-	$MYSQL $server:$port -e "select User,Host from mysql.user where password='' and plugin !='unix_socket' and user !='labsdbuser' and user !='research_role' and user !='mariadb.sys' and host NOT IN ('10.192.0.129','10.192.16.9','10.192.32.137','10.64.16.14','10.64.37.27', '10.64.32.180','10.64.32.179','10.192.48.47','10.64.0.15','10.64.32.10','10.64.16.7','10.64.48.33','10.64.130.15','10.64.134.16','10.64.151.2','10.64.150.4');" -BN | while read user host
-	do
-		echo "set session binlog_format=row; INSERT INTO $TABLE VALUES ('${server}','${port}','${user}','${host}',NOW()) ON DUPLICATE KEY UPDATE last_update = NOW();" | $MYSQL $DB_HOST -u $DB_USER $DATABASE
-	done
-done
+    if [ $USERS_COUNT != 0 ]; then
+        echo "There are users with empty passwords. Please check $DATABASE.$TABLE table on $DB_HOST - this email address isn't monitored" | mail -s "Users with empty passwords detected" sre-data-persistence@wikimedia.org -aFrom:checkusers@wikimedia.org
+    fi
+    ;;
+  --unused-grants)
+    echo "unused-grants report"
 
-
-# Check and report existing users
-# Not sending any kind of username/host on email to avoid any kind hint of over email
-
-USERS_COUNT=$($MYSQL $DB_HOST -u $DB_USER $DATABASE -e "select count(*) from nil_grants;" -BN)
-
-if [ $USERS_COUNT != 0 ]
-then
-	echo "There are users with empty passwords. Please check $DATABASE.$TABLE table on $DB_HOST - this email address isn't monitored"  | mail -s "Users with empty passwords detected" sre-data-persistence@wikimedia.org -aFrom:checkusers@wikimedia.org
-fi
-
- ;;
-
---unused-grants)
-  echo "unused-grants report"
-
-$MYSQL $SOURCE_DB_HOST $SOURCE_DB -e "select server,port from $SOURCE_DB_TABLE;" -BN | while read server port
-do
- echo "======$server:$port======="
- $MYSQL $server:$port -e "select User,Host from mysql.user where host like '208.80.154.%' or host='208.80.152.161' or host='208.80.152.165' or user='globaldev' or user='prefetch' or user='test' or user='repl'" -BN
-done
-
-;;
+    # Fetch servers and ports, check for unused grants
+    $MYSQL $SOURCE_DB_HOST $SOURCE_DB -e "select server,port from $SOURCE_DB_TABLE;" -BN | while read server port
+    do
+        echo "======$server:$port======="
+        $MYSQL $server:$port -e "select User,Host from mysql.user where host like '208.80.154.%' or host='208.80.152.161' or host='208.80.152.165' or user='globaldev' or user='prefetch' or user='test' or user='repl'" -BN
+    done
+    ;;
 esac
+
 exit $RETVAL
